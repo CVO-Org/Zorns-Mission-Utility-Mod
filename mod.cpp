e910da27f766b31cb988f6f3c3f869e8e3909c5f
OverlordZorn <dennisAzorn@gmail.com>
2026-02-25 00:26:01 +0100

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,23 +1,23 @@
-name = "ZRN Mod Template {version}";
+name = "Zorns Mission Utility Mod {version}";
 
 author = "Overlord Zorn";
 
-picture         = "img\flag_voron_red_1024_2048.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+picture         = "img\img_2048.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024 // ToDo
 hideName        = "false";                                                                                                                          // Hide the extension name
 hidePicture     = "false";                                                                                                                          // Hide the extension menu
 
 logoSmall       = "img\Raven_Voron_64.paa";                                                                                                    // Display next to the item added by the mod 64x64
 logo            = "img\logo-voron-white_128.paa";                                                                                                   // Logo displayed in the main menu 128x128
-logoOver        = "img\logo-voron-red_128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+logoOver        = "img\logo-voron-white_128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
 
-tooltip         = "tooltip - If you read this, someone forgot to replace a string in the mod.cpp";
-tooltipOwned    = "tooltipOwned";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
-overview        = "overview";                 // Overview text, displayed from the extension menu
-description     = "description";                                                     // Probably in context with action
-action          = "http://discord.chornivoron.net/";                                                                                                // Website URL, that can accessed from the expansions menu 
-actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
+tooltip         = "Zorns Mission Utility Mod";
+tooltipOwned    = "Zorns Mission Utility Mod";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "Zorns Mission Utility Mod";                                      // Overview text, displayed from the extension menu
+description     = "Zorns Mission Utility Mod";                                      // Probably in context with action
+action          = "https://github.com/CVO-Org/Zorns-Mission-Utility-Mod";           // Website URL, that can accessed from the expansions menu
+actionName      = "Github";                                                         // label of button/tooltip in extension menu
 
-overviewPicture = "voron_round_olive_texture_1024.paa";
+overviewPicture = "img_1024.paa";
 overviewText = "overviewText";
 
 overviewFootnote = "overviewFootnote";
