98828a71d255158d5746f9f9280d0d11812a467f
OverlordZorn <dennisAzorn@gmail.com>
2026-02-24 21:43:34 +0100

    init changes

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -1,7 +1,7 @@
-#define MAINPREFIX ~MAINPREFIX~
-#define PREFIX ~PREFIX~
+#define MAINPREFIX zrn
+#define PREFIX mum
 
-#define PREFIX_BEAUTIFIED ~PREFIX~
+#define PREFIX_BEAUTIFIED Mission Utilty Mod
 
 
 #include "script_version.hpp"
