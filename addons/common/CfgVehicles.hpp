78f79334a7ec249ff919f272d8a6f0c22e95d881
OverlordZorn <dennisAzorn@gmail.com>
2026-08-22 13:24:50 +0200

    fix module locality

--- addons/common/CfgVehicles.hpp
+++ addons/common/CfgVehicles.hpp
@@ -27,7 +27,7 @@ class CfgVehicles {
 
         function = QFUNC(module_healStation);     // Name of function triggered once conditions are met
         functionPriority = 10;                  // Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
-        isGlobal = 1;                           // 0 for server only execution, 1 for global execution, 2 for persistent global execution
+        isGlobal = 2;                           // 0 for server only execution, 1 for global execution, 2 for persistent global execution
         isTriggerActivated = 1;                 // 1 for module waiting until all synced triggers are activated
         isDisposable = 0;                       // 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
         is3DEN = 0;                             // 1 to run init function in Eden Editor as well
@@ -71,7 +71,7 @@ class CfgVehicles {
 
         function = QFUNC(module_makeRemovable);     // Name of function triggered once conditions are met
         functionPriority = 10;                  // Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
-        isGlobal = 1;                           // 0 for server only execution, 1 for global execution, 2 for persistent global execution
+        isGlobal = 2;                           // 0 for server only execution, 1 for global execution, 2 for persistent global execution
         isTriggerActivated = 1;                 // 1 for module waiting until all synced triggers are activated
         isDisposable = 0;                       // 1 if modules is to be disabled once it is activated (i.e. repeated trigger activation will not work)
         is3DEN = 0;                             // 1 to run init function in Eden Editor as well
